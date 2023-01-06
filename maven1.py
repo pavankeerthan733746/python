e8ebbfbee081e657baeae050a1f26cec8ece4ca0
pavankeerthan733746 <61644197+pavankeerthan733746@users.noreply.github.com>
2023-01-06 09:45:50 +0530

    Add files via upload

---
+++ maven1.py
@@ -0,0 +1,31 @@
+n1 = 12
+n2 = 23
+square = n1 * n2
+print("square is:",square)
+
+m=100
+for x in range(1,100):
+    print(m*x)
+
+n=100
+m=120
+print(n+m,n*m,n-m,n%m,n>m,n<m)
+
+
+lista=100
+listb=876
+print(lista+listb)
+
+num=7
+fact=2
+
+for i in range (1,num+1):
+    fact = fact*i
+    print("factorial is:{}".format(fact))
+
+
+
+
+
+
+
